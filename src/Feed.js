93a7583ba4bc6b3356165fdf1151dfcc6c3fb577
ashish-subedi <ashishsubedinp@gmail.com>
2021-09-15 10:27:50 +0545

    InputOptions_l

--- src/Feed.js
+++ src/Feed.js
@@ -3,6 +3,11 @@ import './Feed.css'
 import CreateIcon from "@material-ui/icons/Create"
 import InputOptions from './InputOptions'
 import ImageIcon from "@material-ui/icons/Image"
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import EventNoteIcon from '@material-ui/icons/EventNote';
+import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
+
+
 function Feed() {
     return (
         <div className="feed">
@@ -15,9 +20,10 @@ function Feed() {
         </form>
     </div>
     <div className="feed__inputOptions">
-        <InputOptions Icon={ImageIcon} title="Photo" color="red" />
-        <InputOptions Icon={ImageIcon} title="Video" color="blue" />
-        <InputOptions Icon={ImageIcon} title="Posts" color="Black" />
+        <InputOptions Icon={ImageIcon} title="Photo" color="#7085F9" />
+        <InputOptions Icon={SubscriptionsIcon} title="Video" color="#E7A33E" />
+        <InputOptions Icon={EventNoteIcon} title="Posts" color="#C0CBCD" />
+        <InputOptions Icon={CalendarViewDayIcon} title="Write article" color="#7fc15e" />
 
     </div>
 </div>
